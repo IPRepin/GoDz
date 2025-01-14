43707e99307c2e0d27bd96cf62fe3f1489aae4b1
Pavel Repin <76727704+IPRepin@users.noreply.github.com>
2025-01-14 15:29:14 +0300

    Update main.go

--- ConcurrencyDZ/main.go
+++ ConcurrencyDZ/main.go
@@ -4,31 +4,38 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
-	randNumChan := make(chan []int)
-	resultChan := make(chan []int)
-	go randomNum(randNumChan)
-	randNum := <-randNumChan
-	go exponentiation(resultChan, randNum)
+	randNumChan := make(chan int, 10)
+	resultChan := make(chan string, 10)
+	go func() {
+		for i := 0; i < 10; i++ {
+			randomNum(randNumChan)
+		}
+		close(randNumChan)
+	}()
+	var randSlice []int
+	for randNum := range randNumChan {
+		randSlice = append(randSlice, randNum)
+	}
+	go exponentiation(resultChan, randSlice)
 	fmt.Println(<-resultChan)
 }
 
-func randomNum(ch chan []int) {
-	var randomSliceNums []int
+func randomNum(ch chan int) {
 	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < 10; i++ {
-		randomSliceNums = append(randomSliceNums, rand.Intn(101))
-	}
-	ch <- randomSliceNums
+	numRandom := rand.Intn(101)
+	ch <- numRandom
 }
 
-func exponentiation(ch chan []int, num []int) {
-	results := make([]int, len(num))
-	for i, n := range num {
-		results[i] = int(math.Pow(float64(n), 2))
+func exponentiation(ch chan string, num []int) {
+	var results []string
+	for _, n := range num {
+		results = append(results, strconv.Itoa(int(math.Pow(float64(n), 2))))
 	}
-	ch <- results
+	ch <- strings.Join(results, " ")
 }
