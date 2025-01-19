22b3d6bb42f5d5c9903d066978a6a4e5726c4ceb
Pavel Repin <76727704+IPRepin@users.noreply.github.com>
2025-01-19 11:49:50 +0300

    fix randim-api

--- 2-random-api/main.go
+++ 2-random-api/main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	router := http.NewServeMux()
-	RandomHandler(router)
+	handler := &RandomNum{min: 0, max: 7}
+	RandomHandler(router, handler)
+
 	server := &http.Server{
 		Addr:    ":8081",
 		Handler: router,
 	}
-	fmt.Println("server is listening on :8081")
+
+	log.Println("Сервер слушает на :8081")
 	if err := server.ListenAndServe(); err != nil {
-		fmt.Printf("server error: %v\n", err)
+		log.Fatal("ListenAndServe: ", err)
 	}
 }
--- 2-random-api/router.go
+++ 2-random-api/router.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
 	"net/http"
 	"strconv"
-	"time"
 )
 
-type RandomNum struct{}
+type RandomNum struct {
+	min int
+	max int
+}
 
-func RandomHandler(router *http.ServeMux) {
-	handler := &RandomNum{}
+func RandomHandler(router *http.ServeMux, handler *RandomNum) {
 	router.HandleFunc("/random", handler.RandomNumber())
 }
 
 func (h *RandomNum) RandomNumber() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		rand.Seed(time.Now().UnixNano())
-		numRandom := rand.Intn(7)
-		fmt.Println(numRandom)
-		w.Write([]byte(strconv.Itoa(numRandom)))
+		numRandom := rand.Intn(h.max-h.min) + h.min
+		if _, err := w.Write([]byte(strconv.Itoa(numRandom))); err != nil {
+			http.Error(w, "Ошибка записи в ответ", http.StatusInternalServerError)
+			return
+		}
 	}
 }
