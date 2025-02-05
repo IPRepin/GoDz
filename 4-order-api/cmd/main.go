351eab28ff94364120b9d3308abd3bd45cca7d32
Pavel Repin <76727704+IPRepin@users.noreply.github.com>
2025-02-05 09:18:30 +0300

    add middlewares
    Добавлены middlewares для логирования и CORS

--- 4-order-api/cmd/main.go
+++ 4-order-api/cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"godz/4-order-api/configs"
 	"godz/4-order-api/internal/product"
 	"godz/4-order-api/pkg/db"
+	"godz/4-order-api/pkg/middleware"
 	"net/http"
 )
 
@@ -17,9 +18,15 @@ func main() {
 	product.NewProductHandler(router, product.ProductHandlerDeps{
 		ProductRepo: productRepo,
 	})
+
+	stackMiddlewares := middleware.Chain(
+		middleware.Cors,
+		middleware.Logger,
+	)
+
 	server := &http.Server{
 		Addr:    ":8081",
-		Handler: router,
+		Handler: stackMiddlewares(router),
 	}
 	fmt.Println("server is listening on :8081")
 	if err := server.ListenAndServe(); err != nil {
